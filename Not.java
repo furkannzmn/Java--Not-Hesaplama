379a8d77fb69ad0182dc1fa7669363dd171118dc
furkannzmn <67096139+furkannzmn@users.noreply.github.com>
2020-06-18 21:35:08 +0300

    Create Not.java

---
+++ Not.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+	Scanner s = new Scanner(System.in);
+		
+	System.out.println("Notunuzu girin");
+		
+         int a = s.nextInt();
+		
+	if(90<=a){
+			System.out.println("Notunuz AA");
+		}
+		
+        else if (80<=a){
+			System.out.println("Notunuz BB");
+		}
+		
+	else if(70<=a){
+			System.out.println("Notunuz CC");
+		}
+		
+	
+	else if (60<=a){
+			System.out.println("Notunuz F");
+	
+			}
+}
